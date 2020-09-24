ab28ca3f6b6103bcd52fbb1153c5e307d449f645
jeffrey gaboni <mrgabonijeff@gmail.com>
2020-09-24 08:28:04 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -19,7 +19,7 @@ $(function() {
 	    // how simpleCart should checkout, see the checkout reference for more info
 	    checkout: {
 	        type: "PayPal" ,
-	        email: "you@yours.com"
+	        email: "mrgabonijeff@gmail.com"
 	    },
 
 	    // set the currency, see the currency reference for more info
